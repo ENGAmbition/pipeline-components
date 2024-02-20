225056945411fcca70495a82d1d2e759afdf175a
Tristan Simpson <heytristaann@gmail.com>
2024-02-20 18:32:01 -0500

    feat: add GradientText and PipelineLogoLong components

--- lib/components/index.ts
+++ lib/components/index.ts
@@ -7,3 +7,6 @@ import "../index.css";
  * Export the components
  */
 export { default as Button } from "./Button";
+export { default as GradientText } from "./GradientText";
+export { default as PipelineLogoLong } from "./PipelineLogoLong";
+export { default as PipelineLogoLongBlack } from "./PipelineLogoLongBlack";
